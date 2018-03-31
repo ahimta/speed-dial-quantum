949ce7b8a497f2c0f359dab9a4c7bd165400f77a
Abdullah Alansari <ahimta@gmail.com>
2018-03-31 03:05:54 +0300

    Add some type declarations for better VS Code support :)

---
+++ globals.d.ts
@@ -0,0 +1,6 @@
+interface Window {
+  FileReader: any;
+}
+
+declare var browser: any;
+declare var chrome: any;
--- web-ext-config.js
+++ web-ext-config.js
@@ -2,16 +2,23 @@ module.exports = {
   ignoreFiles: [
     'node_modules',
     'src',
+
     '.editorconfig',
     '.gitignore',
     '.nvmrc',
     '.prettierrc',
     '.travis.yml',
+
+    'globals.d.ts',
+    'jsconfig.json',
+
     'LICENSE',
     'README.md',
+
     'background.js.map',
     'content.js.map',
     'newtab.js.map',
+
     'packages.json',
     'rollup.config.js',
     'yarn.lock'
