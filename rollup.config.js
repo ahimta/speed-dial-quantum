100ded804fde3364175073dbbd76ede8510b4bea
Abdullah Alansari <ahimta@gmail.com>
2018-04-13 17:34:17 +0300

    Fix watch script doesn't work for private files/modules:sweat_smile:

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 export default [
   {
     input: 'src/background.js',
-    output: { file: 'background.js', format: 'iife', sourcemap: true }
+    output: { file: 'background.js', format: 'iife', sourcemap: true },
+    watch: { include: 'src/_*.js' }
   },
   {
     input: 'src/content.js',
-    output: { file: 'content.js', format: 'iife', sourcemap: true }
+    output: { file: 'content.js', format: 'iife', sourcemap: true },
+    watch: { include: 'src/_*.js' }
   },
   {
     input: 'src/newtab.js',
-    output: { file: 'newtab.js', format: 'iife', sourcemap: true }
+    output: { file: 'newtab.js', format: 'iife', sourcemap: true },
+    watch: { include: 'src/_*.js' }
   }
 ]
