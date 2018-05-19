c5e35d94b42dff38f7cd61d97142c028b379dcbc
Abdullah Alansari <ahimta@gmail.com>
2018-05-19 05:27:52 +0300

    Fix watching file doesn't always work:sweat_smile:
    Watch for all source files (less than a dozen):sweat_smile:

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,21 @@ export default [
   {
     input: 'src/background.js',
     output: { file: 'dist/background.js', format: 'iife', sourcemap: true },
-    watch: { include: 'src/_*.js' }
+    watch: { include: 'src/*.js' }
   },
   {
     input: 'src/content.js',
     output: { file: 'dist/content.js', format: 'iife', sourcemap: true },
-    watch: { include: 'src/_*.js' }
+    watch: { include: 'src/*.js' }
   },
   {
     input: 'src/newtab.js',
     output: { file: 'dist/newtab.js', format: 'iife', sourcemap: true },
-    watch: { include: 'src/_*.js' }
+    watch: { include: 'src/*.js' }
   },
   {
     input: 'src/popup.js',
     output: { file: 'dist/popup.js', format: 'iife', sourcemap: true },
-    watch: { include: 'src/_*.js' }
+    watch: { include: 'src/*.js' }
   }
 ]
