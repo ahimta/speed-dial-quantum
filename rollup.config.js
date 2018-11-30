c9714ede167575b07155f576879bef3f2a0a093b
Abdullah Alansari <ahimta@gmail.com>
2018-11-30 14:33:34 +0300

    refactor(build): DRY out Rollup config:smiley:

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,6 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 
 const childProcess = require('child_process')
 
-const plugins = [nodeResolve({ jsnext: true, main: true }), commonjs({})]
-
-const name = 'x'
-const format = 'iife'
-const sourcemap = true
-const watch = {
-  include: ['src/**/*.js']
-}
-
 const environment = process.env.NODE_ENV || 'development'
 const version = childProcess
   .execSync('git rev-parse HEAD')
@@ -21,28 +12,26 @@ const version = childProcess
 const intro = `window.SDQ = { environment: '${environment}', version: '${version}' }`
 
 export default [
-  {
-    plugins,
-    watch,
-    input: 'src/browser/background.js',
-    output: { name, format, sourcemap, intro, file: 'dist/background.js' }
-  },
-  {
-    plugins,
-    watch,
-    input: 'src/browser/content.js',
-    output: { name, format, sourcemap, intro, file: 'dist/content.js' }
-  },
-  {
-    plugins,
-    watch,
-    input: 'src/browser/newtab.js',
-    output: { name, format, sourcemap, intro, file: 'dist/newtab.js' }
-  },
-  {
-    plugins,
-    watch,
-    input: 'src/browser/popup.js',
-    output: { name, format, sourcemap, intro, file: 'dist/popup.js' }
-  }
+  getConfig('src/browser/background.js', 'dist/background.js'),
+  getConfig('src/browser/content.js', 'dist/content.js'),
+  getConfig('src/browser/newtab.js', 'dist/newtab.js'),
+  getConfig('src/browser/popup.js', 'dist/popup.js')
 ]
+
+function getConfig (inputPath, outputPath) {
+  const plugins = [nodeResolve({ jsnext: true, main: true }), commonjs({})]
+
+  const name = 'x'
+  const format = 'iife'
+  const sourcemap = true
+  const watch = {
+    include: ['src/**/*.js']
+  }
+
+  return {
+    plugins,
+    watch,
+    input: inputPath,
+    output: { name, format, sourcemap, intro, file: outputPath }
+  }
+}
