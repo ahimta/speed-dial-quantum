79607ea2046d673179088865c235a57cd1522259
Abdullah Alansari <ahimta@gmail.com>
2020-01-24 14:42:09 +0300

    chore: do static assets upgrades
    1. Upgrade Bootstrap
    2. Remove Popper. Seems to be only needed for Bootstrap's Popover which
    isn't used.
    3. Move Raven/Sentry to NPM packages.

--- src/browser/newtab.js
+++ src/browser/newtab.js
@@ -1,3 +1,5 @@
+const Sentry = require('@sentry/browser')
+
 const dialView = require('./_dial_view')
 const repos = require('../repos')
 const render = require('../ui')
@@ -5,19 +7,18 @@ const render = require('../ui')
 // @ts-ignore
 const { environment, version: release } = window && window.SDQ
 
-window.Raven.config(
-  'https://1e7c5d9a607b48de9ce21c3807df07f0@sentry.io/1246981',
-  { environment, release }
-).install()
-
-window.Raven.context(() => {
-  const view = dialView()
-
-  document.addEventListener('keydown', view.keydown)
-  document.addEventListener('mousedown', view.mousedown)
-  document.addEventListener('keyup', view.keyup)
-  ;(async () => {
-    const groups = await repos.group.list()
-    render(groups[0].id)
-  })()
+Sentry.init({
+  dsn: 'https://1e7c5d9a607b48de9ce21c3807df07f0@sentry.io/1246981',
+  environment,
+  release
 })
+
+const view = dialView()
+
+document.addEventListener('keydown', view.keydown)
+document.addEventListener('mousedown', view.mousedown)
+document.addEventListener('keyup', view.keyup)
+;(async () => {
+  const groups = await repos.group.list()
+  render(groups[0].id)
+})()
